12a51057ab49597485938a2f5fabea03d478eb00
rohit_singh12 <119621966+rohit5349@users.noreply.github.com>
2026-05-07 13:48:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 
 app.use(cors({
-  origin: ["https://taskora-l1kcbo6lt-rohits-projects-3e8dba6f.vercel.app"],  
+  origin: [
+    "https://taskora-p4dgazm4b-rohits-projects-3e8dba6f.vercel.app",
+    "http://localhost:5173"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
