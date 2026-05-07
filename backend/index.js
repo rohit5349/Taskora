c05a0c3d8d8209f39683f33ccc0fccff4ce32bd8
rohit_singh12 <119621966+rohit5349@users.noreply.github.com>
2026-05-07 13:26:29 +0530

    fix vercel cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://taskoraapp-git-main-rohits-projects-3e8dba6f.vercel.app",  
+  origin: ["https://taskora-l1kcbo6lt-rohits-projects-3e8dba6f.vercel.app"],  
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
