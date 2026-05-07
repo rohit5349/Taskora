3e68566f40a394e7e651b15815837bc12b5d6be2
rohit_singh12 <119621966+rohit5349@users.noreply.github.com>
2026-05-07 12:47:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",  
+  origin: "https://taskoraapp.vercel.app",  
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
